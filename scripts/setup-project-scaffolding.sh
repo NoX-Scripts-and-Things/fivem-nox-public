cb46ad20e48882878ba413858db215c6ccba74f9
N0xiously <melcomve@outlook.com>
2023-09-22 19:48:36 +0200

    Latest

--- scripts/setup-project-scaffolding.sh
+++ scripts/setup-project-scaffolding.sh
@@ -49,5 +49,19 @@ else
   cd ../.. || exit 1
 fi
 
+
+# Pull the latest fivem nox server, this will do all the work to create a fivem server with the correct settings to test the mod with
+if [ ! -d ".source/fivem-nox-server" ]
+then
+    echo "Nox public code does not exist, pulling repository"
+    cd .source || exit 1
+    git clone git@github.com:NoX-Scripts-and-Things/fivem-nox-server.git
+    cd .. || exit 1
+else
+  cd .source/fivem-nox-server || exit 1
+  git pull origin master
+  cd ../.. || exit 1
+fi
+
 # Execute boilerplate, if the fxmanifest.lua does not exist then a boilerplate framework will be generated
 source ./.source/fivem-nox-public/scripts/create-starter-code.sh
