f2bd35c7caecfed65e6805a227f792d2d1146179
N0xiously <melcomve@outlook.com>
2023-09-23 15:44:03 +0200

    Latest

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,22 +18,22 @@ curl -o refresh-source.sh https://raw.githubusercontent.com/NoX-Scripts-and-Thin
 
 if [ "$FRAMEWORK" == "qb-core" ]
 then
+  curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/qb-core/write-template.sh | bash
+
   sed -i '/FRAMEWORK=/d' refresh-source.sh
   sed -i "2a\\FRAMEWORK=\"qb-core\"" refresh-source.sh
 
   sed -i '/FRAMEWORK=/d' start-server.sh
   sed -i "2a\\FRAMEWORK=\"qb-core\"" start-server.sh
-
-  curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/qb-core/write-template.sh | bash
 fi
 
 if [ "$FRAMEWORK" == "esx" ]
 then
+  curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/esx/write-template.sh | bash
+
   sed -i '/FRAMEWORK=/d' refresh-source.sh
   sed -i "2a\\FRAMEWORK=\"esx\"" refresh-source.sh
 
   sed -i '/FRAMEWORK=/d' start-server.sh
   sed -i "2a\\FRAMEWORK=\"esx\"" start-server.sh
-
-  curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/esx/write-template.sh | bash
 fi
