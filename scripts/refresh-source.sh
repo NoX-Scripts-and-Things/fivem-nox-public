d78c52c752eb2d2bef0e9564e7d3f8816084cd4c
N0xiously <melcomve@outlook.com>
2023-09-23 15:49:44 +0200

    Latest

--- scripts/refresh-source.sh
+++ scripts/refresh-source.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FRAMEWORK="$1"
+source .env
 
 git init
 
@@ -14,26 +14,14 @@ curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-pub
 
 curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/fivem-nox-server.sh | bash
 
-curl -o refresh-source.sh https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/init.sh
+curl -o refresh-source.sh https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/refresh-sources.sh
 
 if [ "$FRAMEWORK" == "qb-core" ]
 then
   curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/qb-core/write-template.sh | bash
-
-  sed -i '/FRAMEWORK=/d' refresh-source.sh
-  sed -i "2a\\FRAMEWORK=\"qb-core\"" refresh-source.sh
-
-  sed -i '/FRAMEWORK=/d' start-server.sh
-  sed -i "2a\\FRAMEWORK=\"qb-core\"" start-server.sh
 fi
 
 if [ "$FRAMEWORK" == "esx" ]
 then
   curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/templates/esx/write-template.sh | bash
-
-  sed -i '/FRAMEWORK=/d' refresh-source.sh
-  sed -i "2a\\FRAMEWORK=\"esx\"" refresh-source.sh
-
-  sed -i '/FRAMEWORK=/d' start-server.sh
-  sed -i "2a\\FRAMEWORK=\"esx\"" start-server.sh
 fi
