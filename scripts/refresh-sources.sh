e92c7f91d81774f15176804ba6bddaea00f673a7
N0xiously <melcomve@outlook.com>
2023-09-26 08:56:12 +0200

    Latest

--- scripts/refresh-sources.sh
+++ scripts/refresh-sources.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
-if [ -n "$1" ]
-then
+if [ -e .source/.framework ]; then
+  echo "[INFO] Loading framework from '.source/.framework' ..."
+  source .source/.framework
+
+elif [ -n "$1" ]; then
+  echo "[INFO] Received framework argument, setting FRAMEWORK=$1"
   FRAMEWORK=$1
-else
-  source .env
+
+  if [ -n "$FRAMEWORK" ]; then
+    echo "[INFO] Writing framework to cache '.source/.framework'"
+    echo "FRAMEWORK=\"$FRAMEWORK\"" > .source/.framework
+  fi
 fi
 
-if [ -z "$FRAMEWORK" ]
-then
-  echo "Framework not set!"
+if [ -z "$FRAMEWORK" ]; then
+  echo "[ERROR] Unable to determine framework!"
   exit 1
 fi
 
 git init
 
 curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/fivem-structure.sh | bash
-
-echo "FRAMEWORK=\"$FRAMEWORK\"" > .source/.framework
 
 curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/fivem-nox-public.sh | bash
 
