1fa641eaa42aa783e8d237451891ff521ce8cf8a
N0xiously <melcomve@outlook.com>
2023-10-13 20:30:53 +0200

    Latest

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p .source
 git clone git@github.com:NoX-Scripts-and-Things/fivem-nox-public.git .source/fivem-nox-public
 ./.source/fivem-nox-public/src/prepare-env.sh ".source/fivem-nox-public/requirements.txt"
 
-# python .source/fivem-nox-public/init.py "$1"
+python .source/fivem-nox-public/init.py "$1"
 
 
 
