561b64e7db959f7855a2c9a52ccadbd9ed259efc
N0xiously <melcomve@outlook.com>
2023-10-14 22:37:29 +0200

    Latest

--- windows/gta-v/qb-core.sh
+++ windows/gta-v/qb-core.sh
@@ -1,18 +1,17 @@
-# git init
+git init
 
-# mkdir -p .source
-# git clone git@github.com:NoX-Scripts-and-Things/fivem-nox-public.git .source/fivem-nox-public
-
+mkdir -p .source
+git clone git@github.com:NoX-Scripts-and-Things/fivem-nox-public.git .source/fivem-nox-public
 
 pip3 install virtualenv
 
-# (
-#   cd ./.source/fivem-nox-public || exit 1
-#   python3 -m venv venv
-#   source venv/Scripts/activate
-#   pip install -r "requirements.txt"
-#   GIT_PYTHON_GIT_EXECUTABLE="/mingw64/bin/git" python init.py "qb-core"
-# )
+(
+  cd ./.source/fivem-nox-public || exit 1
+  python3 -m venv venv
+  source venv/Scripts/activate
+  pip install -r "requirements.txt"
+  GIT_PYTHON_GIT_EXECUTABLE="/mingw64/bin/git" python init.py "qb-core"
+)
 
 (
   cd ./.source/fivem-nox-server || exit 1
@@ -23,20 +22,19 @@ pip3 install virtualenv
   ./.source/fivem-nox-server/src/build-server.sh
 )
 
-# (
-#   cd ./.source/fivem-nox-testing-suite || exit 1
-#   python3 -m venv venv
-#   source venv/Scripts/activate
-#   pip install -r "requirements.txt"
-# )
+(
+  cd ./.source/fivem-nox-testing-suite || exit 1
+  python3 -m venv venv
+  source venv/Scripts/activate
+  pip install -r "requirements.txt"
+)
 
-# (
-#   cd ./.source/fivem-nox-core || exit 1
-#   python3 -m venv venv
-#   source venv/Scripts/activate
-#   pip install -r "requirements.txt"
-# )
-
+(
+  cd ./.source/fivem-nox-core || exit 1
+  python3 -m venv venv
+  source venv/Scripts/activate
+  pip install -r "requirements.txt"
+)
 
 
 # if [ "$1" == "copy-server-files" ]; then
