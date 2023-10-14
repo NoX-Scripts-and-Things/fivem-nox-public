d223103083af23e921dacec57b9db402c37cfc22
N0xiously <melcomve@outlook.com>
2023-10-14 23:14:40 +0200

    Latest

--- test-framework.py
+++ test-framework.py
@@ -1,8 +1,6 @@
 import sys
 
-from src.lib.framework import cache_framework
 from src.lib.importer import import_source
-from src.lib.template import template_generator
 
 import_source("fivem-nox-public", ".source")
 import_source("fivem-nox-testing-suite", ".source")
