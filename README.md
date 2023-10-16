1d164c5ed79a9c3b54d41e638b730390246b01e6
N0xiously <melcomve@outlook.com>
2023-10-16 18:28:30 +0200

    Latest

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ The libraries contain the following:
 
 
 ```
-curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/setup/initialize.sh | bash
+curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/initialize.sh | bash -s -- init-base.py qb-core
 ```
--- src/init-base.py
+++ src/init-base.py
@@ -6,28 +6,30 @@ from lib.template import template_generator
 
 framework: str = sys.argv[1]
 
-if framework != "qb-core" and framework != "esx":
-    print("Unsupported framework provided: {}".format(framework))
-    exit()
+print("awe")
 
-cache_framework(framework, source_dir="../")
-
-if framework == "qb-core":
-    # Project Directory
-    import_source("fivem-nox-core", "..")
-    import_source("fivem-nox-server", "..")
-    import_source("fivem-nox-testing-suite", "..")
-    # import_source("fivem-nox-public", "..")
-
-    # Source Code: fivem-nox-core
-    import_source("fivem-nox-testing-suite", "../fivem-nox-core/.source")
-
-    # Import public into the other source directories
-    import_source("fivem-nox-public", "../fivem-nox-core/.source")
-    import_source("fivem-nox-public", "../fivem-nox-server/.source")
-    import_source("fivem-nox-public", "../fivem-nox-testing-suite/.source")
-
-    template_generator(dest_directory="..\\..",
-                       source_directory="..\\..\\.source",
-                       template_directory="src\\templates\\{}",
-                       force=True)
+# if framework != "qb-core" and framework != "esx":
+#     print("Unsupported framework provided: {}".format(framework))
+#     exit()
+#
+# cache_framework(framework, source_dir="../")
+#
+# if framework == "qb-core":
+#     # Project Directory
+#     import_source("fivem-nox-core", "..")
+#     import_source("fivem-nox-server", "..")
+#     import_source("fivem-nox-testing-suite", "..")
+#     # import_source("fivem-nox-public", "..")
+#
+#     # Source Code: fivem-nox-core
+#     import_source("fivem-nox-testing-suite", "../fivem-nox-core/.source")
+#
+#     # Import public into the other source directories
+#     import_source("fivem-nox-public", "../fivem-nox-core/.source")
+#     import_source("fivem-nox-public", "../fivem-nox-server/.source")
+#     import_source("fivem-nox-public", "../fivem-nox-testing-suite/.source")
+#
+#     template_generator(dest_directory="..\\..",
+#                        source_directory="..\\..\\.source",
+#                        template_directory="src\\templates\\{}",
+#                        force=True)
