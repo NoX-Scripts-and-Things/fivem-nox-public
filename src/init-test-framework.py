548425122abc714c3ae1b6cefc77e743b3c40975
N0xiously <melcomve@outlook.com>
2023-10-16 17:08:34 +0200

    Latest

--- src/init-test-framework.py
+++ src/init-test-framework.py
@@ -1,6 +1,6 @@
 import sys
 
-from src.lib.importer import import_source
+from lib.importer import import_source
 
 import_source("fivem-nox-public", ".source")
 import_source("fivem-nox-testing-suite", ".source")
