aee6a258885a7103f0775cc3fe95707d56f271bc
N0xiously <melcomve@outlook.com>
2023-10-16 18:04:50 +0200

    Latest

---
+++ setup/initialize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+INITIAL_SCRIPTS="$1"
+ARG="$2"
+
+# shellcheck disable=SC1090
+source <(curl -s https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/src/scripts/prepare-scripts.sh)
+# shellcheck disable=SC1090
+source <(curl -s https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/src/scripts/prepare-env.sh)
+
+git init
+
+prepare_scripts
+
+(
+  cd ./.source/fivem-nox-public || exit 1
+  activate_python
+  GIT_PYTHON_GIT_EXECUTABLE="$(which git)" python ./src/"$INITIAL_SCRIPTS" "$ARG"
+)
--- src/init-test-framework.py
+++ src/init-test-framework.py
@@ -2,5 +2,5 @@ import sys
 
 from lib.importer import import_source
 
-import_source("fivem-nox-public", ".source")
-import_source("fivem-nox-testing-suite", ".source")
+import_source("fivem-nox-public", "..")
+import_source("fivem-nox-testing-suite", "..")
