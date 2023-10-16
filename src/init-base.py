b1f5c366c46bb599f375f35f583c133835d233d8
N0xiously <melcomve@outlook.com>
2023-10-16 18:40:34 +0200

    Latest

--- src/init-base.py
+++ src/init-base.py
@@ -5,6 +5,8 @@ from lib.framework import cache_framework
 from lib.importer import import_source
 from lib.template import template_generator
 
+print(str(len(sys.argv)))
+
 framework = sys.argv[1] if len(sys.argv) >= 1 else None
 internal = sys.argv[2].lower() == "true" if len(sys.argv) >= 2 else False
 
