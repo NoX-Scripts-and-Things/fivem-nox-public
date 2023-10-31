5d8c6f7ecea54e5dd2d1254327aa0f5040b283bc
N0xiously <melcomve@outlook.com>
2023-10-31 02:26:57 +0100

    Latest

--- tests/test_entire_flow.sh
+++ tests/test_entire_flow.sh
@@ -7,7 +7,7 @@ mkdir "$TARGET_TEST_DIR"
 cd $TARGET_TEST_DIR || exit 1
 
 # Run the initialization script with qb-core as the base
-curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/initialize.sh | bash -s -- init-base.py qb-core
+curl -sSL https://raw.githubusercontent.com/NoX-Scripts-and-Things/fivem-nox-public/master/scripts/initialize.sh | bash -s -- base qb-core
 
 # Remove the .gitignore file in the test directory and replace it with * to disable anything in that dir from being pushed to the repo as a .git is initialized in the test directory
 rm -rf ".gitignore"
